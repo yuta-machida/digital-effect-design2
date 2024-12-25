9629c0a0ca9860f0accdf1ec9549cca610f1f827
yuta_machida <yuta.machida@g.dreamcareer.co.jp>
2024-12-25 16:56:01 +0900

    スライドアニメーション調整完了

--- src/assets/ts/animation/slideAnimation.ts
+++ src/assets/ts/animation/slideAnimation.ts
@@ -1,11 +1,13 @@
 import { animationWrapper, mediaQuery } from "../variable";
 
-// div要素を作成する関数
-function createSlideAnimationContainer(imgUrl?: string): HTMLDivElement[] {
-  const slideBox: HTMLDivElement[] = [];
+const NUMBER_OF_BOXES = 5;
+
+// スライドさせる要素を生成し、背景色を追加する
+function createSlideBox() {
   const slideAnimationContainer: HTMLElement = document.createElement("div");
   slideAnimationContainer.classList.add("mv__slide-animation-container");
   animationWrapper?.appendChild(slideAnimationContainer);
+  const slideBox: HTMLDivElement[] = [];
   const color: string[] = [
     "#4B8A70",
     "#56927A",
@@ -13,25 +15,11 @@ function createSlideAnimationContainer(imgUrl?: string): HTMLDivElement[] {
     "#6E9A81",
     "#8BAC97",
   ];
-  const NUMBER_OF_BOXES = 5;
-
   for (let i: number = 0; i < NUMBER_OF_BOXES; i++) {
     const div = document.createElement("div");
     div.classList.add("mv__slide-box");
-    //　↓↓ 画面幅によって変更　↓↓
-    if (imgUrl) {
-      div.style.backgroundImage = `url(${imgUrl})`;
-      mediaQuery.matches
-        ? (div.style.backgroundPosition = `${
-            (i * 100) / (NUMBER_OF_BOXES - 1)
-          }% 0`)
-        : (div.style.backgroundPosition = `center ${
-            (i * 100) / (NUMBER_OF_BOXES - 1)
-          }%`);
-    }
-    if (color) {
-      div.style.backgroundColor = color[i];
-    }
+    div.style.backgroundColor = color[i];
+
     if (mediaQuery.matches) {
       i % 2 !== 0
         ? (div.style.transform = "translateY(100%)")
@@ -41,35 +29,66 @@ function createSlideAnimationContainer(imgUrl?: string): HTMLDivElement[] {
         ? (div.style.transform = "translateX(100%)")
         : (div.style.transform = "translateX(-100%)");
     }
-    // ↑↑ 画面幅によって変更　↑↑
     slideAnimationContainer?.appendChild(div);
     slideBox.push(div);
   }
   return slideBox;
 }
 
+// image要素を生成して、clip-pathで切り抜き
+function createSlideImage(imageURL: string) {
+  const slideAnimationContainer: HTMLElement = document.createElement("div");
+  slideAnimationContainer.classList.add("mv__slide-animation-container");
+  animationWrapper?.appendChild(slideAnimationContainer);
+  const slideBox: HTMLDivElement[] = [];
+  let firstPosition: number = 0;
+  let lastPosition: number = 20;
+  for (let i: number = 0; i < NUMBER_OF_BOXES; i++) {
+    const image = document.createElement("img");
+    image.setAttribute("src", imageURL);
+    image.classList.add("mv__slide-image");
+
+    if (mediaQuery.matches) {
+      image.style.clipPath = `polygon(${firstPosition}% 0%,${lastPosition}% 0%,${lastPosition}% 100%,${firstPosition}% 100%)`;
+      i % 2 !== 0
+        ? (image.style.transform = "translateY(100%)")
+        : (image.style.transform = "translateY(-100%)");
+    } else {
+      image.style.clipPath = `polygon(0% ${firstPosition}%, 100% ${firstPosition}%, 100% ${lastPosition}%,0% ${lastPosition}%)`;
+      i % 2 !== 0
+        ? (image.style.transform = "translateX(100%)")
+        : (image.style.transform = "translateX(-100%)");
+    }
+    firstPosition += 20;
+    lastPosition += 20;
+    slideAnimationContainer?.appendChild(image);
+    slideBox.push(image);
+  }
+  return slideBox;
+}
+
 // スライドアニメーション
-export function slideAnimation(imgUrl?: string): void {
-  const div: HTMLDivElement[] = createSlideAnimationContainer(imgUrl);
+export function slideAnimation(imgURL?: string): void {
+  const slideBox = imgURL ? createSlideImage(imgURL) : createSlideBox();
   const duration: number = 100;
   let animationDelay: number = 0;
 
-  for (let i: number = 0; i < div.length; i++) {
+  for (let i: number = 0; i < slideBox.length; i++) {
     if (i % 2 !== 0) {
-      div[i].animate(
+      slideBox[i].animate(
         mediaQuery.matches
           ? [{ transform: "translateY(100%)" }, { transform: "translateY(0)" }]
-          : [{ transform: "translateX(100%)" }, { transform: "translateX(0)" }], // 画面幅によって変更
+          : [{ transform: "translateX(100%)" }, { transform: "translateX(0)" }],
         { duration: duration, delay: animationDelay, fill: "forwards" }
       );
     } else {
-      div[i].animate(
+      slideBox[i].animate(
         mediaQuery.matches
           ? [{ transform: "translateY(-100%)" }, { transform: "translateY(0)" }]
           : [
               { transform: "translateX(-100%)" },
               { transform: "translateX(0)" },
-            ], // 画面幅によって変更
+            ],
         { duration: duration, delay: animationDelay, fill: "forwards" }
       );
     }
